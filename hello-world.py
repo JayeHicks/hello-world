83c574e592b9e159ba3a918ba2e2fe9848365191
jaye-hicks <jaye.hicks@gmail.com>
2025-05-25 15:31:56 -0500

    initial commit

---
+++ hello-world.py
@@ -0,0 +1,15 @@
+"""
+A simple Python program that prints "hello world" to standard output.
+"""
+
+def hello_world():
+    """Function that prints the hello world message."""
+    print("hello world")
+
+def main():
+    """Main function that calls the hello-world function."""
+    hello_world()
+
+if __name__ == "__main__":
+    main()
+    
