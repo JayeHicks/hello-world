742aa1c1f34fa6c48290ae0df1f2c2cead7693c5
jaye-hicks <jaye.hicks@gmail.com>
2025-05-25 15:42:59 -0500

    got the snake case naming correct finally

--- hello_world.py
+++ hello_world.py
@@ -2,13 +2,13 @@
 A simple Python program that prints "hello world" to standard output.
 """
 
-def HelloWorld():
+def hello_world():
     """Function that prints the hello world message."""
     print("hello world")
 
 def main():
     """Main function that calls the hello-world function."""
-    HelloWorld()
+    hello_world()
 
 if __name__ == "__main__":
     main()
