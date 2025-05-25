ecb5921db99001c49abec453f87ffb81a6748a01
jaye-hicks <jaye.hicks@gmail.com>
2025-05-25 15:37:06 -0500

    renamed file and function to follow snake case

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,13 +2,13 @@
 A simple Python program that prints "hello world" to standard output.
 """
 
-def hello_world():
+def HelloWorld():
     """Function that prints the hello world message."""
     print("hello world")
 
 def main():
     """Main function that calls the hello-world function."""
-    hello_world()
+    HelloWorld()
 
 if __name__ == "__main__":
     main()
